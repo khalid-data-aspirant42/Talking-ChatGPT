75a5b08f5fb3055732b7680d5e46314b22b668d7
MD Khalid Ansari <102864888+khalid-data-aspirant42@users.noreply.github.com>
2023-04-03 15:30:47 +0530

    More features added

--- talking_chatgpt.py
+++ talking_chatgpt.py
@@ -19,10 +19,20 @@ import speech_recognition as sr
 # import translation library
 from googletrans import Translator
 
+# import google news libraries
+from GoogleNews import GoogleNews as GN
+import pandas as pd
+
+# for image generation
+import requests
+from bs4 import BeautifulSoup
+import urllib.request
+import os
+from PIL import Image
 '''----------------------------------api and model---------------------------'''
 
 # Set up the OpenAI API client :
-openai.api_key = "your api"
+openai.api_key = "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"
 
 # Set up the model (more models, visit https://beta.openai.com/playground)
 model_engine = "text-davinci-003"
@@ -30,24 +40,26 @@ model_engine = "text-davinci-003"
 
 '''---------------------------------book of functions--------------------------'''
 
-# Define a function that sends a message to ChatGPT
-def chat_query(prompt):
-    completions = openai.Completion.create(
+# creating chatGPT class
+class chatGPT:
+    # Define a function that sends a message to ChatGPT
+    def chat_query(self, prompt):
+        completions = openai.Completion.create(
         engine=model_engine,
         prompt=prompt,
-        max_tokens=2048,
+        max_tokens=512,
         n=1,
         temperature=0.5,
-    )
+        )
 
-    message = completions.choices[0].text
-    return message
+        message = completions.choices[0].text
+        return message
 
-# Define a function that handles the conversation
-def conversation_handler(prompt):
-    # Send the prompt to ChatGPT
-    response = chat_query(prompt)
-    return response
+    # Define a function that handles the conversation
+    def conversation_handler(self, prompt):
+        # Send the prompt to ChatGPT
+        response = self.chat_query(prompt)
+        return response
 
 ### Creating function to summarize long sentence
 def summarize(text, n=3):
@@ -124,6 +136,67 @@ def takeCommand():
         return "None" #None string will be returned
     return query
 
+# function to get temperature of a location
+def getTemperture(location):
+    import requests
+    from bs4 import BeautifulSoup   
+    # Enter the City Name
+    search = "Temperture in {}".format(location)
+ 
+    # URL
+    url = f"https://www.google.com/search?&q={search}"
+  
+    # Sending HTTP request
+    req = requests.get(url)
+ 
+    # Pulling HTTP data from internet
+    sor = BeautifulSoup(req.text, "html.parser")
+          
+    # Getting the required data
+    data = sor.find("div", class_='BNeawe').text
+    print(f'The temperature in {location} is {data}')
+    speak(f'The temperature in {location} is {data}')
+
+
+# ------
+# function to get google news
+def getNews(location):
+    news = GN(period='1d')
+    news.search(location)
+    result = news.result()
+    data = pd.DataFrame.from_dict(result)
+    return data
+
+
+# generate image
+def gen_image(query):
+    # Set the directory where you want to save the images
+    directory = "images"
+
+    # Create the directory if it doesn't exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Search for images
+    url = f"https://www.google.com/search?q={query}&tbm=isch&tbs=ic:specific,isc:white,isz:lt,islt:qsvga"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+    image_tags = soup.find_all("img")
+
+    # Download the images to the local directory
+    for i, image in enumerate(image_tags[3:4]):
+        url = image["src"]
+        if not url.startswith("http"):
+            url = f"https://www.google.com{url}"
+        filename = f"{query.replace(' ', '_')}_{i}.jpg"
+        filepath = os.path.join(directory, filename)
+        urllib.request.urlretrieve(url, filepath)
+        # Open the downloaded image file
+        img = Image.open(filepath)
+        # Show the image
+        img.show()
+
+
 '''-----------------------------main program starts here-------------------------'''
 
 ### getting voice input
@@ -133,42 +206,110 @@ r = sr.Recognizer()
 # chat gpt working
 if __name__ == "__main__":
   # Start the conversation
-  speak("Hello! I am your personal assistant. What do you want to know?")
-  query3 = 'yes'
+  speak("Hello! I am your personal assistant. How can i help you?")
+  query6 = takeCommand().lower()
+  if '' in query6:
+      speak('Here is a list of task available')
+      speak('Current affairs such as news, temperature, download images or any explanation from Chat GPT')
+  else:
+      speak('Here is a list of task available')
+      speak('Current affairs such as news, temperature, image download or any explanation from Chat GPT')
+
+  query7 = takeCommand().lower()
   while(True):
-      if 'yes' in query3:
-        speak('Ask your question')
-        query1 = takeCommand().lower()
-        speak('Please choose: summary or description')
-        query2 = takeCommand().lower()      
-      
-        print('Processing your task. Please wait....')  
-        if 'summary' in query2:
-            sen = conversation_handler(query1)
-            summary = summarize(sen,n=2)
-            speak('Whether you want written summary or verbal summary')
-            query4 = takeCommand().lower()
-            if 'written' in query4:
-                speak('Here is your written summary')
-                print(summary)
-            else:
-                print(summary)
-                speak(summary)
-        else:
-            sen = conversation_handler(query1)
-            speak('Do you want written description or verbal description?')
-            query5 = takeCommand().lower()
-            if 'written' in query5:
-                speak('Here is your written description')
-                print(sen)
-            else:
-                print(sen)
-                speak(sen)
+      if ('news' in query7):
+          speak('Please tell me the location: city, state or country')
+          location = takeCommand().lower()
+          news = getNews(location)
+          speak("Do you want to listen to the news or wanna read only?")
+          query9 = takeCommand().lower()
+          if 'listen' in query9:
+              speak(f"Here are the today's top 5 news of{location}")
+              x = 1
+              for elem in news['title']:
+                  print(elem)
+                  speak(f'News number {x}:')
+                  speak(elem)
+                  x+=1
+                  if x==6:
+                      break
+          else:
+              speak(f"Here are the today's top 5 news of{location}")
+              print(news)
+          speak('Do you want to know news of some other places. If not, you can choose temperature, image download or connect to chat gpt, or do nothing')
+          query7 = takeCommand().lower()
+          if 'nothing' in query7:
+              print('Thank you for your questions. Have a nice day')
+              speak('Thank you for your questions. Have a nice day')
+              break
+
+      if ('temperature' in query7):
+          speak('please tell me the location: city, state or country')
+          query8 = takeCommand().lower()
+          temp = getTemperture(query8)
+          print(temp)
+          speak('Do you want to know temperature of some other place. If not, you can choose news, image download or connect to chat gpt, or do nothing')
+          query7 = takeCommand().lower()
+          if 'nothing' in query7:
+              print('Thank you for your questions. Have a nice day')
+              speak('Thank you for your questions. Have a nice day')
+              break
+          
+      if ('download' in query7):
+          speak('please tell me which image you want to download')
+          query10 = takeCommand().lower()
+          img1 = gen_image(query10)
+          print(img1)
+          speak('Do you want to download some more image. If not, you can choose news, temperature or connect to chat gpt, or do nothing')
+          query7 = takeCommand().lower()
+          if 'nothing' in query7:
+              print('Thank you for your questions. Have a nice day')
+              speak('Thank you for your questions. Have a nice day')
+              break
+          
+      if ('chat gpt' in query7):
+            #   ------
+            query3 = 'yes'
+            while(True):
+                if 'yes' in query3:
+                    speak('Ask your question')
+                    query1 = takeCommand().lower()
+                    speak('Please choose: summary or description')
+                    query2 = takeCommand().lower()      
+                    # creating an instance for chatGPT class named talkGPT
+                    talkGPT = chatGPT()
+                    print('Processing your task. Please wait....')  
+                    if 'summary' in query2:
+                        sen = talkGPT.conversation_handler(query1)
+                        summary = summarize(sen,n=2)
+                        speak('Whether you want written summary or verbal summary')
+                        query4 = takeCommand().lower()
+                        if 'written' in query4:
+                            speak('Here is your written summary')
+                            print(summary)
+                        else:
+                            print(summary)
+                            speak(summary)
+                    else:
+                        sen = talkGPT.conversation_handler(query1)
+                        speak('Do you want written description or verbal description?')
+                        query5 = takeCommand().lower()
+                        if 'written' in query5:
+                            speak('Here is your written description')
+                            print(sen)
+                        else:
+                            print(sen)
+                            speak(sen)
         
-        speak('Would you like to continue?')
-        query3 = takeCommand().lower()
+                    speak('Would you like to continue with chat GPT?')
+                    query3 = takeCommand().lower()
               
-      elif 'no' in query3:
-        print('Thank you for your questions. Have a nice day.')
-        speak('Thank you for your questions. Have a nice day.')
-        break    
+                elif 'no' in query3:
+                    break    
+                    #   ------  
+            speak('what else i can do: temperature or news or nothing')
+            query7 = takeCommand().lower()
+            if 'nothing' in query7:
+                print('Thank you for your questions. Have a nice day')
+                speak('Thank you for your questions. Have a nice day')
+                break
